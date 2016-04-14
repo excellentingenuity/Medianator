a25de97a789162f21ef1df43eb807708c1d899f3
James Johnson <james.johnson@excellentingenuity.com>
2016-04-14 07:33:25 -0400

    composer changes

---
+++ src/Mediantor.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace eig\Medianator;
+
+
+class Mediantor
+{
+    // given a file and a filesystem adapter save a media file to the filesystem
+    
+}
---
+++ tests/MediantorTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace eig\Medianator\Tests;
+
+
+class MediantorTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp ()
+    {
+        parent::setUp();
+    }
+
+    public function tearDown ()
+    {
+        parent::tearDown();
+    }
+}
